5cd43b3bdb5bfd5dfcaa9b20de1ed20ed4852649
mray <mail@mray.de>
2012-10-20 00:18:51 +0200

    tes

--- index.php
+++ index.php
@@ -692,7 +692,7 @@ $app->get("/participate", function() {
 $app->get("/search", function() {
     return new Template('main', array( 
         function() {return array('content' => array(new Template('search',null)));
-        }, 'class' => 'search');
+        }, array('class' => 'search'));
 });
 
 $app->get('/test', function() {
