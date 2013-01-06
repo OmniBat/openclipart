b84274e329b190b6819949b6ab1bd0058f12e3b8
Jakub Jankiewicz <jcubic@onet.pl>
2013-01-06 13:16:19 +0100

    Dynamic Tags

--- index.php
+++ index.php
@@ -44,9 +44,9 @@ require_once('libs/OCAL.php');
 
 $app = new OCAL(array(
     'db_prefix' => 'openclipart',
-    'tag_limit' => 100,
+    'tag_limit' => 30,
     'top_artist_last_month_limit' => 10,
-    'home_page_thumbs_limit' => 8,
+    'home_page_thumbs_limit' => 9,
     'home_page_collections_limit' => 5,
     'home_page_news_limit' => 3,
     'token_expiration' => 1, // number of hours for token expiration (token send via email)
@@ -403,6 +403,16 @@ $app->get("/user/:username", function($username) use ($app) {
 
 $app->get("/", function() {
     return new Template('main', function() {
+        // tags
+        global $app;
+        $query = "SELECT count(openclipart_tags.id) as tag_count  FROM openclipart_clipart_tags INNER JOIN openclipart_tags ON openclipart_tags.id = tag GROUP BY tag ORDER BY tag_count DESC LIMIT 1";
+        $max = $app->db->get_value($query);
+        $query = "SELECT openclipart_tags.name, count(openclipart_tags.id) as tag_count FROM openclipart_clipart_tags INNER JOIN openclipart_tags ON openclipart_tags.id = tag GROUP BY tag ORDER BY tag_count DESC LIMIT " . $app->config->tag_limit;
+        $result = array();
+        $rows = $app->db->get_array($query);
+        shuffle($rows);
+        $normalize = size('20', $max);
+
         return array(
             'editable' => false, // librarian functions
             'login-dialog' => new Template('login-dialog', null),
@@ -420,7 +430,14 @@ $app->get("/", function() {
                     return array(
                         'clipart_list' => $app->list_clipart(null, "created")
                     );
-                })
+                }),
+                'tags' =>
+                     array_map(function($row) use ($normalize) {
+                         return array(
+                             'name' => $row['name'],
+                             'size' => $normalize($row['tag_count'])
+                         );
+                     }, $rows)
             )),
             'social-box' => new Template('social_boxes', null)
         );
