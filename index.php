4fe1fcf29d6e766b7bc5b63eec9add74e5acc3d5
mray <mail@mray.de>
2013-01-16 13:55:00 +0100

    add profile template

--- index.php
+++ index.php
@@ -716,6 +716,12 @@ $app->get("/why-the-ads", function() {
     });
 });
 
+$app->get("/profile", function() { //added just to be able to work on css. can be removed once profiles are available
+    return new Template('main', function() {
+        return array('content' => array(new Template('profile', null)));
+    });
+});
+
 $app->get("/search", function() {
     return new Template('main', function() {
         return array(
