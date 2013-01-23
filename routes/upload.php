c5cd310a027278f8f29cec9650b1ac92f5108cdc
Victor Powell <vicapow@gmail.com>
2013-01-23 12:23:04 -0800

    better component intergration for client side code. added additional upload functionality

--- routes/upload.php
+++ routes/upload.php
@@ -4,4 +4,10 @@ $app->get("/upload", function() use($app){
   return $app->render('upload');
 });
 
+$app->post("/upload", function() use($app){
+  if(!$app->is_logged()) return $app->redirect('/');
+  var_dump($_FILES);
+  var_dump($_POST);
+});
+
 ?>
