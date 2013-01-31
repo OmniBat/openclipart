9c0e4c0f9e4ae65ba8769dd406181885e5dbc005
Victor Powell <vicapow@gmail.com>
2013-01-31 01:15:38 -0800

    removed tags page. all tags now link to search page. also added links for /artists route

--- routes/tags.php
+++
@@ -1,9 +0,0 @@
-<?php
-$app->get("/tags/:tag", function($tag) use($app) {
-  return $app->render('tags', array(
-    'cliparts' => $app->clipart_by_tag($tag)
-    , 'tags' => array($tag)
-  ));
-});
-
-?>
