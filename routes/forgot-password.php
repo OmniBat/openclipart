2f1eaff78dbef136dcf0e2bf8099885b14ea18a3
Victor Powell <vicapow@gmail.com>
2013-02-21 20:22:57 -0800

    built out forgot-password view and route.

--- routes/forgot-password.php
+++ routes/forgot-password.php
@@ -1,20 +1,29 @@
 <?php
-$app->map('/forgot-password', function() use ($app) {
-    if(!isset($_GET['email'])) 
-        return $app->render('forgot-password');
-    $email = $_GET['email'];
-    if ($app->send_reset_password_link($email, $app->config->token_expiration)) {
-        $msg = "Instant access link was send to your email";
-        $error = false;
-    } else {
-        $msg = "We couldn't send an email, maybe you put wrong email adress";
-        $error = true;
-    }
-    if ($app->request()->isAjax()){
-        return json_encode(array('result' => $msg, 'error' => $error));
-    }else{
-        return $app->render('main', array('content' => $msg));
-    }
-})->via('GET', 'POST');
+
+$app->get('/forgot-password', function() use($app){
+  return $app->render('forgot-password');
+});
+
+$app->post('/forgot-password', function() use($app){
+  if(empty($_POST['email'])) return $app->notFound();
+  $email = $_POST['email'];
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    return $app->render('forgot-password', array(
+      'errors' => array(
+        'email' => 'invalid email address'
+      )
+    ));
+  if ($app->send_reset_password_link($email, $app->config->token_expiration)){
+    $msg = "Instant access link was send to your email!";
+    $error = false;
+  }else{
+    $msg = "No user exists with that email address.";
+    $error = true;
+  }
+  return $app->render('forgot-password', array(
+    'msg' => $msg
+    , 'errors' => $error
+  ));
+});
 
 ?>
