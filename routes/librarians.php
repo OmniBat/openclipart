5d11b112b48fdfedd88fd21ad842ded584866ded
Victor Powell <vicapow@gmail.com>
2013-03-02 23:58:20 -0500

    fixed bug with the display of avatars. 'id' attr collision in sql

--- routes/librarians.php
+++ routes/librarians.php
@@ -1,10 +1,10 @@
 <?php
 
 $app->get('/librarians', function() use($app){
-    return $app->render('librarians', array(
-      'librarians' => $app->get_users_by_group('librarian')
-      , 'developers' => $app->get_users_by_group('admin')
-   ));
+  return $app->render('librarians', array(
+    'librarians' => $app->get_users_by_group('librarian')
+    , 'developers' => $app->get_users_by_group('admin')
+  ));
 });
 
 ?>
